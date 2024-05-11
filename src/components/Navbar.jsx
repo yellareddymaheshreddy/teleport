8e67dd51737b792e1fe33629c58d307cab4f1b75
Mahesh Reddy <yellareddymaheshreddy@gmail.com>
2024-05-11 21:37:14 +0530

    menu for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,38 @@ import { useNavigate } from 'react-router-dom'
 const Navbar = () => {
   const authStatus = useSelector((state) => state.auth.status)
   const navigate = useNavigate();
+  const [menu, setmenu] = React.useState(false)
 
   return (
     <div className="relative w-full bg-white">
+      {menu&&<div className='absolute right-[23px] top-[80px] bg-[#f6f6f6] rounded-xl p-8 w-[12rem] pink-shadow font-bold'>
+        <ul className="menu bg-base-200 w-fit rounded-box flex flex-col gap-4">
+          <li>
+            <NavLink className={({ isActive }) => `flex gap-2 items-center hover:text-fuchsia-600 ${isActive? "text-fuchsia-600" : ""}`} to={"/"} onClick={() => { setmenu(false) }}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => `flex gap-2 items-center hover:text-fuchsia-600 ${isActive? "text-fuchsia-600" : ""}`} to={"/about"} onClick={() => { setmenu(false) }}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => `flex gap-2 items-center hover:text-fuchsia-600 ${isActive? "text-fuchsia-600" : ""}`} to={"/projects"} onClick={() => { setmenu(false) }}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => `flex gap-2 items-center hover:text-fuchsia-600 ${isActive? "text-fuchsia-600" : ""}`} to={"/contact"} onClick={() => { setmenu(false) }}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+              Contact Us
+            </NavLink>
+          </li>
+        </ul>
+      </div>}
       <div className="mx-auto flex min-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8 min-h-24">
         <div className="inline-flex items-center space-x-2">
           <span>
@@ -70,7 +99,7 @@ const Navbar = () => {
               Sign Up
             </button></span>}
         </div>
-        <div className="lg:hidden">
+        <div className="lg:hidden" onClick={() => { setmenu(!menu)}}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
