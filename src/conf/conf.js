db35814a7ea07403a85020629ce3448d4665084f
Mahesh Reddy <yellareddymaheshreddy@gmail.com>
2024-05-13 16:10:37 +0530

    added to env

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,11 +4,11 @@ const conf={
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    SecureToken: "7833a18c-3c81-4383-9707-7fb7d7d6e319",
-    maheshmail:'mahesh.contactus@gmail.com',
-    mailjsserviceid:'service_je28889',
-    mailjstemplate:'template_fp4emog',
-    mailjspublickey:'fURV-yVGhcU617Pkm',
+    SecureToken: String(import.meta.env.VITE_SECURETOKEN),
+    maheshmail:String(import.meta.env.VITE_MAHESHMAIL),
+    mailjsserviceid:String(import.meta.env.VITE_MAILJSSERVICEID),
+    mailjstemplate:String(import.meta.env.VITE_MAILJSTEMPLATE_ID),
+    mailjspublickey:String(import.meta.env.VITE_MAILJSKEY),
 
 }
 
